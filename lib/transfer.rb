6241d12a1be676432a9bcbf278cc8932070593a2
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-18 07:21:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :status
   attr_reader :sender, :receiver, :amount
-
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
@@ -16,6 +16,28 @@ class Transfer
   def execute_transaction
     (both_valid? && @status == "pending" && @sender.balance > @amount) ? accept_transfer : reject_transfer
   end
+
+  def accept_transfer
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  end
+
+  def reject_transfer
+    @status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+
+end
+
 
   def accept_transfer
     @sender.balance -= @amount
